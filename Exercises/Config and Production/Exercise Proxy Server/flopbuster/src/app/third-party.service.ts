a39de4bd25e75aecb0ac6ddc45e70b0b2254bc62
Joe Eames <joeeames@gmail.com>
2020-05-16 16:38:42 -0600

    added 3rdparty service

---
+++ Exercises/Config and Production/Exercise Proxy Server/flopbuster/src/app/third-party.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThirdPartyService {
+
+  constructor() {
+
+  }
+
+  login(username: string, password: string) {
+
+  }
+
+}
